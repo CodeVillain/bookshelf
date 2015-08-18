33ef1bc1b504236d2aa1394912a763b30baef015
Yuriy Kabay <yuriy.kabay@gmail.com>
2015-08-18 15:51:32 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var bookArray = [
+    {
+        author: 'Chuck Palahniuk',
+        title: 'Fight Club',
+        genre: 'thriller'
+    },
+    {
+        author: 'Isaac Asimov',
+        title: 'The End of Eternity',
+        genre: 'fiction'
+    },
+    {
+        author: 'Ray Bradbury',
+        title: 'Fahrenheit 451',
+        genre: 'fiction'
+    },
+    {
+        author: 'Fyodor Dostoyevsky',
+        title: 'The Idiot',
+        genre: 'classics'
+    },
+    {
+        author: 'Jason Fried',
+        title: 'Rework',
+        genre: 'business'
+    }
+];
